f9e5fd91e42592da5b8758fbf4916a4483d66d6c
Daniel Scholl (MS] <daniel.scholl@microsoft.com>
2025-04-30 13:53:07 +0000

    Add Workload Identity support to MSI authentication

--- src/main/java/org/opengroup/osdu/azure/di/MSIConfiguration.java
+++ src/main/java/org/opengroup/osdu/azure/di/MSIConfiguration.java
@@ -2,11 +2,13 @@ package org.opengroup.osdu.azure.di;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * A configuration bean class Enable/Disable MSI (Managed Service Identity).
+ * This class also checks for Workload Identity configuration as an alternative authentication method.
  */
 @Configuration
 @ConfigurationProperties(prefix = "azure.msi")
@@ -14,4 +16,15 @@ import org.springframework.context.annotation.Configuration;
 @Setter
 public class MSIConfiguration {
     private Boolean isEnabled = false;
+
+    @Autowired
+    private WorkloadIdentityConfiguration workloadIdentityConfiguration;
+
+    /**
+     * Checks if either MSI or Workload Identity authentication is enabled.
+     * @return true if either MSI or Workload Identity is enabled, false otherwise
+     */
+    public Boolean getIsEnabled() {
+        return isEnabled || workloadIdentityConfiguration.getIsEnabled();
+    }
 }
